e4487c86a471cdcaecaef5ff606bac9dc78be57b
sohel5G <105277301+sohel5G@users.noreply.github.com>
2023-09-26 21:24:22 +0600

    Some responsive issues fixed on mobile version

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = ({ getSearchValue }) => {
 
     return (
         <div className="custom-bg-img bg-no-repeat bg-cover bg-top center">
-            <div className="custom-bg-overlay h-[300px] md:h-[400px] lg:h-[600px] flex justify-center items-center pt-24">
+            <div className="custom-bg-overlay h-[400px] md:h-[500px] lg:h-[600px] flex justify-center items-center py-5 md:pt-20 lg:pt-24">
                 <div className="container mx-auto px-10 ">
                     <h1 className="text-3xl lg:text-5xl text-center font-bold py-10">I Grow By Helping People In Need</h1>
                     <form className="flex justify-center items-center" onSubmit={handleSearch}>
